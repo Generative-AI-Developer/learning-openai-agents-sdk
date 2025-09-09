cad783ff33a1789b19505118b77b1e9b70ccd6e8
Muhammad Asif <programmerasif@hotmail.com>
2025-09-09 12:29:09 +0500

    handsoff advance added

---
+++ handsoff_advance.py
@@ -0,0 +1,35 @@
+from agents import Agent, Runner, ModelSettings, handoff
+from dotenv import load_dotenv
+load_dotenv()
+import asyncio
+
+
+
+refund_agent = Agent(
+    name="Refund agent",
+    instructions="You are a helpful assistant",
+    model="gpt-5-nano",
+    model_settings=ModelSettings(max_tokens=400)  # Limit response length
+
+    
+)
+
+general_agent = Agent(
+    name="General agent",
+    instructions=(
+        "You are a helpful assistant. If the user's request is about a refund, "
+        " handoff to the Refund agent."
+        "Otherwise, respond generally."
+    ),
+    handoffs=[handoff(agent=refund_agent)],
+    model="gpt-5-nano",
+    # model_settings=ModelSettings(max_tokens=400)  # Limit response length
+)
+
+async def main():
+    result = await Runner.run(general_agent, "hi")
+    print(result.final_output)
+    print("last agent:", result.last_agent.name)
+
+if __name__ == "__main__":
+    asyncio.run(main())
